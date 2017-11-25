7568c555195b223a8daadd9082a46bc72a5f3696
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-25 10:58:51 -0600

    added clients to array so we can actually work with them individually

--- index.js
+++ index.js
@@ -61,9 +61,31 @@ app.post('/item/new', (req, res) => {
       redisClient.hgetall(`items${number}`, (err, object) => {
         itemsArray.push(object);
         res.render(`items/${number + 1}`);
+
+        //emit new auction event so the client gets realtime uptdates currently does nothing
+        io.emit('new-auction', object);
       });
     },
   );
+});
+//A sample client is written to sampleClientArray.txt, just so we can remember what properties the clients have
+let clientArray = [];
+
+//handle websockets. I have the client emitting an event to identify the page they are on, the clients are storred in an array and we can handle them with standard control flow
+//We don't want every single page to add new items, as that would add items to the pages designed for viewing a specific item
+io.on('connection', client => {
+  console.log(client);
+  client.on('id-event', location => {
+    //location will give us both the absolute path (location.href) and the relative path location.pathname for the client
+    client.clientLocation = location;
+    clientArray.push(client);
+  });
+
+  //removes the client from the array when they disconnect
+  client.on('disconnect', () => {
+    let index = clientArray.indexOf(client);
+    clientArray.splice(index, 1);
+  });
 });
 
 server.listen(3000);
